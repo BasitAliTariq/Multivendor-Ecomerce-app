377e5d3ced2da30caecf9620bcffae571d4e69fc
BasitAliTariq <basitalitariq9@gmail.com>
2026-05-09 12:24:21 +0500

    add frontend url in app.j

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,9 @@ const isProd = process.env.NODE_ENV === "production";
 
 app.use(
   cors({
-    origin: isProd ? process.env.FRONTEND_URL : "http://localhost:5173",
+    origin: isProd
+      ? "https://multivendor-ecomerce-app-x299.vercel.app"
+      : "http://localhost:5173",
     credentials: true,
   }),
 );
